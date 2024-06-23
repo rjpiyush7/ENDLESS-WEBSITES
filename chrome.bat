d5431215c2561322fbc6e29fbd44153082bc0eed
Piyush Pramodsingh Rajput <rj14piyush@gmail.com>
2024-06-23 18:40:19 +0530

    Add files via upload

---
+++ chrome.bat
@@ -0,0 +1,12 @@
+@echo off
+:A
+start chrome "https://www.youtube.com"
+start chrome "https://www.facebook.com"
+start chrome "https://www.instagram.com"
+start chrome "https://www.youtube.com"
+start chrome "https://www.facebook.com"
+start chrome "https://www.instagram.com"
+start chrome "https://www.youtube.com"
+start chrome "https://www.facebook.com"
+start chrome "https://www.instagram.com"
+goto A
